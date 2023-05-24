f782bdc298a58ab19f16c5feb1c206793239cc50
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2023-05-24 14:19:50 -0500

    Se creó el archivo funciones.js y se implemento la función para listar archivos, se enlazó el archivo funciones.js con el index.html

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ app.listen(3000, () => {
 app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'index.html'))
 });
+
+app.get('/listarArchivos', (request, response) => {
+	fs.readdir('priv', (error, archivos) => {
+		if (error) {
+			console.error(error);
+			response.status(500).json({
+                error: 'Error'
+            });
+			return;
+		}
+		response.json({
+            archivos
+        });
+	});
+  });
---
+++ pub/funciones.js
@@ -0,0 +1,17 @@
+function mostrarArchivos(archivos) {
+    const listaArchivos = document.getElementById('main');
+    archivos.forEach((archivo) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = archivo;
+      listaArchivos.appendChild(listItem);
+    });
+  }
+
+function verArchivos(){
+    const url = 'http://localhost:3000/listarArchivos'
+    fetch(url).then(
+        response => response.json()
+    ).then(
+        data => mostrarArchivos(data.archivos)
+    )
+}
